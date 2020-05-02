68bf6fc5256d6e50cdce19bf73dfdc42e7d328b5
Beau Jackson <beau.html@gmail.com>
2020-05-02 00:52:16 -0400

    color

--- src/components/content/flipscreen.js
+++ src/components/content/flipscreen.js
@@ -13,12 +13,12 @@ const FlipScreen = () => (
     <FlipScrnWrap xmlns="http://www.w3.org/2000/svg" id="screen" x="0" y="0" version="1.1" viewBox="0 0 300 300" preserveAspectRatio="none">
 
         <radialGradient id="flipscreen-grad" cx="150" cy="150" r="180" gradientUnits="userSpaceOnUse">
-            <stop offset="0" stopColor="#a17342" />
-            <stop offset=".1432" stopColor="#a37848" />
-            <stop offset=".3355" stopColor="#a78659" />
-            <stop offset=".5556" stopColor="#ae9d75" />
-            <stop offset=".7945" stopColor="#b8be9c" />
-            <stop offset=".9992" stopColor="#c2dec3" />
+        <stop offset="0" stopColor="#a17342"/>
+    <stop offset=".136" stopColor="#a47848"/>
+    <stop offset=".3186" stopColor="#ad8559"/>
+    <stop offset=".5276" stopColor="#bb9c75"/>
+    <stop offset=".7564" stopColor="#cfbb9c"/>
+    <stop offset=".9992" stopColor="#e8e2ce"/>
         </radialGradient>
         <path fill="url(#flipscreen-grad)" d="M0 0h300v300H0z" />
         <g id="lines">
