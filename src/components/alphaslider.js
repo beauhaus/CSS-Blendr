7e300a6c142f62c47348476695bdad95e1ac6f2f
Beau Jackson <beau.html@gmail.com>
2020-06-07 19:29:08 -0400

    alphaslider test on mobile

---
+++ src/components/alphaslider.js
@@ -0,0 +1,128 @@
+import React, { useState, useEffect } from 'react'
+import styled from 'styled-components';
+import ReactSlider from 'react-slider'
+
+const StyledSlider = styled(ReactSlider)`
+    height: 80vh;
+    width: 20%;
+    margin-left: 2vw;
+    background: slategrey;
+    border: 1px solid #fff;
+`;
+
+const StyledThumb = styled.div`
+    width: 100%;
+    height: 4vh;
+    line-height: 35px;
+    text-align: center;
+    background-color: pink;
+    color: #777;
+    cursor: grab;    
+`;
+
+const Thumb = (props, state) => <StyledThumb {...props}>{state.valueNow}%</StyledThumb>;
+
+const StyledTrack = styled.div`
+    background: #888;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    background: rgba(255, 255, 255, ${props=> props.alpha/100 });
+`;
+
+const Track = (props, state) => {
+console.log("Tp&s: ", state.value)
+    return (
+        <StyledTrack
+            trackClassName="example-track"
+            {...props}
+            alpha={state.value}
+            index={state.index}
+        />
+    )
+};
+
+const AlphaSlider = () => {
+
+
+
+    const changeHandler = (val => {
+        return (val)
+    })
+
+    const beforeChangeHandler = (val => {
+        console.log("B4 Change: ", val)
+        return (val)
+    })
+    const afterChangeHandler = (val => {
+        console.log("After Change: ", val)
+        return (val)
+    })
+    return (
+        <StyledSlider
+            className="vertical-slider"
+            defaultValue={[50]}
+            orientation="vertical"
+            renderTrack={Track}
+            renderThumb={Thumb}
+            onBeforeChange={val => beforeChangeHandler(val)}
+            onAfterChange={val => afterChangeHandler(val)}
+            onChange={val => changeHandler(val)}
+        />
+    )
+}
+
+export default AlphaSlider;
+
+{/* <ReactSlider
+    className="horizontal-slider"
+    thumbClassName="example-thumb"
+    trackClassName="example-track"
+    onBeforeChange={val => console.log('onBeforeChange value:', val)}
+    onChange={val => console.log('onChange value:', val)}
+    onAfterChange={val => console.log('onAfterChange value:', val)}
+    renderThumb={(props, state) => <div {...props}>{state.valueNow}</div>}
+/> 
+
+
+___________________________________________________
+
+const StyledSlider = styled(ReactSlider)`
+    width: 100%;
+    height: 25px;
+`;
+
+const StyledThumb = styled.div`
+    height: 25px;
+    line-height: 25px;
+    width: 25px;
+    text-align: center;
+    background-color: #000;
+    color: #fff;
+    border-radius: 50%;
+    cursor: grab;
+`;
+
+const Thumb = (props, state) => <StyledThumb  thumbClassName="example-thumb" {...props}>{state.valueNow}</StyledThumb>;
+
+const StyledTrack = styled.div`
+    top: 0;
+    bottom: 0;
+    background: ${props => props.index === 2 ? '#f00' : props.index === 1 ? '#0f0' : '#ddd'};
+    border-radius: 999px;
+`;
+
+const Track = (props, state) => <StyledTrack {...props} index={state.index} />;
+
+<StyledSlider
+    defaultValue={[50, 75]}
+    renderTrack={Track}
+    renderThumb={Thumb}
+/>
+
+ useEffect(() => {
+        
+    console.log("hello!")
+    }, [])
+*/}
--- src/pages/page-b.js
+++ src/pages/page-b.js
@@ -1,25 +1,19 @@
-import React  from 'react';
+import React from 'react';
 import styled from 'styled-components';
-// // import ColorDodgeIcon from '../components/content/colordodgeicon';
-// import Glitch from '../components/content/vertglitchtest'
+import Slider from '../components/alphaslider'
 
-// // import Glitchmp4 from "../../static/images/glitchvid.mp4"
-// import TestVid from '../components/testVid'
 const PageBWrapper = styled.div`
 box-shadow: inset 0 0 0 4px slategrey;
-
-
-}
-
+background: slategrey;
 `
 
 const PageB = (props) => {
-  
-  
+
   return (
     <PageBWrapper className="page-b-wrapper">
-    PageB
-      </PageBWrapper>
+      PageB
+      <Slider />
+    </PageBWrapper>
   )
 };
 
