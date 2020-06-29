403a9425e67e26096604af508aced32234a083a3
Beau Jackson <beau.html@gmail.com>
2020-06-29 01:18:26 -0400

    glitch1

--- src/components/test/refactor/imagepairs.js
+++ src/components/test/refactor/imagepairs.js
@@ -1,8 +1,8 @@
 import React, { useContext } from 'react';
 import styled from 'styled-components';
 
-import {AppContext} from '../../../pages/page-b'
-// import Glitch from '../../content/vertglitch'
+import { AppContext } from '../../../pages/page-b'
+import Glitch from '../../../../images/images_util/newglitch2.jpg'
 import Eye from '../../../../images/gallery-images/eye.jpg'
 import Rouge from '../../../../images/gallery-images/rouge.jpg'
 import Woman from '../../../../images/gallery-images/woman.jpg'
@@ -15,6 +15,7 @@ const StyledImagePairs = styled.div`
     height: 100%;
     z-index: 0;
     background: linear-gradient(135deg, steelblue 0%, PeachPuff 25%, PeachPuff 50%, LightSalmon 75%, steelblue 100%);
+    overflow: hidden;
     .img1, .img2 {
         position: absolute;
         top: 0;
@@ -26,28 +27,34 @@ const StyledImagePairs = styled.div`
         position: absolute;
         top: 0;
         left: 0;
-        width: 100%;
+        width: 2700px;
         height: 100%;
-        animation: glitchAnim 200ms steps(10) forwards;
-
+        
+        animation: glitchAnim 500ms steps(10) forwards;
+        mix-blend-mode: color-burn; //exclusion
  }
  @keyframes glitchAnim {
-	${'' /* from { transform: translate(0%, 0%) }
-	to { transform: translate(-100%, 0%)}   */}
-        0% { opacity:0; }
-        50% { opacity:1; }
-        100% { opacity:0; }
+        0% {
+             opacity:1;
+             transform: translate(0%, 0%);
+             }
+        50% {
+             opacity:1; 
+             }
+        100% {
+             opacity:0;
+             transform: translate(-100%, 0%);
+             }
     }
 `
 
 const ImagePairs = () => {
-    const {addImageMode } = useContext(AppContext);
-    return (!addImageMode? <StyledImagePairs className="image-pairs">
-        <img className="img1" src={Eye} alt="testtesttesttest"/>
-        <img className="img2" src={Rouge} alt="test2test2test2test2"/>
-        <img className="glitch-img" src={Woman} alt="test3test3test3test3"/>
-        
-    </StyledImagePairs>:'')
+    const { addImageMode } = useContext(AppContext);
+    return (!addImageMode ? <StyledImagePairs className="image-pairs">
+        <img className="img1" src={Eye} alt="testtesttesttest" />
+        <img className="img2" src={Rouge} alt="test2test2test2test2" />
+        <img className="glitch-img" src={Glitch} alt="test3test3test3test3" />
+    </StyledImagePairs> : '')
 };
 
 export default ImagePairs; 
